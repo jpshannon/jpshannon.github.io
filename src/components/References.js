863e1baaa0a3500790d0e95068d6b1fdc1c930c7
Joshua Shannon <jshannon421@gmail.com>
2018-05-17 12:51:19 -0500

    converte references section

--- src/components/References.js
+++ src/components/References.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Section from './Section';
+import Experience from './Experience';
 
-export default function References() {
-	return null;
+export default function References(props) {
+	if (props.resume.references.length === 0) {
+		return null;
+	}
+	return (
+		<Section className="references" icon="thumbs-up" title="References">
+			{props.resume.references.map((item,idx) =><Section title={item.name}>{item.reference}</Section>)}
+		</Section>
+	)
 }
