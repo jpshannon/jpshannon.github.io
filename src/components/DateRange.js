9f261fdc1c4e2b42c32c0742d98145e8ebd4a268
Joshua Shannon <jshannon421@gmail.com>
2018-05-17 12:43:59 -0500

    converted volunteer section

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -4,8 +4,8 @@ import Moment from 'react-moment';
 export default function DateRange (props) {
 	const format = "MMM YYYY";
 	const dateSpan = !props.hideDateSpan && props.end ? <Moment className="date-span" from={props.end} ago>{props.start}</Moment> : '';
-	const end = props.end ? (<Moment className="end-date" format={format}>{props.end}</Moment>) : 'Present';
-	const divider = props.end ? ' - ' : '';
+	const end = props.end ? (<Moment className="end-date" format={format}>{props.end}</Moment>) : <time>Present</time>;
+	const divider = ' - ';
 	return (
 		<div>
 			<Moment className="start-date" format={format}>{props.start}</Moment>
--- src/components/Section.js
+++ src/components/Section.js
@@ -6,8 +6,6 @@ export default function Section (props) {
 	if(!header && props.title) {
 		header = <SectionTitle title={props.title} icon={props.icon} />
 	}
-	console.log("Title %s", props.title);
-	console.log("Header %o", header);
 	const SectionType = props.article ? 'article' : 'section';
 	return (
 		<SectionType className={props.className}>
